bf0f64bf4079b1926b4b4a916ea98fe45a9e4bed
Erim Icel <erim.icel@olioex.com>
2026-04-29 10:26:21 +0100

    Update configuration_spec.rb

--- spec/typesense/configuration_spec.rb
+++ spec/typesense/configuration_spec.rb
@@ -29,4 +29,47 @@ describe Typesense::Configuration do
       end
     end
   end
+
+  describe '#num_retries default' do
+    let(:base_options) do
+      {
+        api_key: 'abcd',
+        nodes: [
+          { host: 'node0', port: 8108, protocol: 'http' },
+          { host: 'node1', port: 8108, protocol: 'http' },
+          { host: 'node2', port: 8108, protocol: 'http' }
+        ],
+        log_level: Logger::ERROR
+      }
+    end
+
+    it 'defaults to the number of nodes when no nearest_node is set' do
+      config = described_class.new(base_options)
+      expect(config.num_retries).to eq(3)
+    end
+
+    it 'defaults to nodes.length + 1 when a nearest_node is set' do
+      config = described_class.new(
+        base_options.merge(nearest_node: { host: 'nearestNode', port: 6108, protocol: 'http' })
+      )
+      expect(config.num_retries).to eq(4)
+    end
+
+    it 'reflects node count for single-node setups' do
+      config = described_class.new(
+        base_options.merge(nodes: [{ host: 'node0', port: 8108, protocol: 'http' }])
+      )
+      expect(config.num_retries).to eq(1)
+    end
+
+    it 'honors an explicit num_retries option' do
+      config = described_class.new(base_options.merge(num_retries: 7))
+      expect(config.num_retries).to eq(7)
+    end
+
+    it 'honors an explicit num_retries of 0 (Integer is truthy in Ruby)' do
+      config = described_class.new(base_options.merge(num_retries: 0))
+      expect(config.num_retries).to eq(0)
+    end
+  end
 end
